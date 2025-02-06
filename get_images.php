c65780a98669449657265a4eda5df2f22d2539a3
RipalAF <sub2planetmilo@gmail.com>
2025-02-06 13:26:07 +0700

    menambahkan folder berisi foto-foto cctv dikapal
    menambahkan file login dan logout

--- get_images.php
+++ get_images.php
@@ -1,25 +1,45 @@
 <?php
 $folder = 'images/';
-$directories = array_diff(scandir($folder), ['.', '..']);
 $categories = [];
 
-// Loop ke dalam setiap subfolder
-foreach ($directories as $dir) {
-    $subfolderPath = $folder . $dir . '/';
-    if (is_dir($subfolderPath)) {
-        $files = scandir($subfolderPath);
-        foreach ($files as $file) {
-            if (in_array(pathinfo($file, PATHINFO_EXTENSION), ['jpg', 'jpeg', 'png'])) {
-                preg_match('/_ch(\d+)/', $file, $matches);
-                if (isset($matches[1])) {
-                    $category = 'ch' . $matches[1];
-                    $categories[$category][] = [
-                        'path' => $subfolderPath . $file,
-                        'name' => $dir . ' - ' . $file // Tambahkan nama subfolder sebelum nama file
-                    ];
-                }
-            }
+// Fungsi untuk scan direktori secara rekursif
+function scanDirectoryRecursively($dir) {
+    $files = [];
+    if (!is_dir($dir)) {
+        return $files;
+    }
+    
+    $scan = scandir($dir);
+    foreach ($scan as $item) {
+        if ($item === '.' || $item === '..') {
+            continue;
         }
+        
+        $path = $dir . DIRECTORY_SEPARATOR . $item;
+        
+        if (is_dir($path)) {
+            // Jika folder, lakukan rekursi
+            $files = array_merge($files, scanDirectoryRecursively($path));
+        } elseif (in_array(pathinfo($path, PATHINFO_EXTENSION), ['jpg', 'jpeg', 'png'])) {
+            $files[] = $path;
+        }
+    }
+    return $files;
+}
+
+// Ambil semua file gambar secara rekursif
+$allFiles = scanDirectoryRecursively($folder);
+
+// Kelompokkan gambar berdasarkan kategori (chX)
+foreach ($allFiles as $file) {
+    preg_match('/_ch(\d+)/', $file, $matches);
+    if (isset($matches[1])) {
+        $category = 'ch' . $matches[1];
+        $subfolderName = basename(dirname($file)); // Nama subfolder tempat gambar berada
+        $categories[$category][] = [
+            'path' => $file,
+            'name' => $subfolderName . ' - ' . basename($file)
+        ];
     }
 }
 
@@ -50,7 +70,7 @@ $paginatedFiles = array_slice($categoryFiles, $startIndex, $itemsPerPage);
 foreach ($paginatedFiles as $fileData) {
     echo "<div class='border p-2 bg-white shadow-lg rounded-lg transform transition-transform duration-300 hover:scale-105'>";
     echo "<img src='{$fileData['path']}' alt='{$fileData['name']}' class='w-full h-48 object-cover rounded-t-lg'>";
-    echo "<p class='text-center mt-2 text-gray-700'>{$fileData['name']}</p>"; // Nama subfolder + nama file
+    echo "<p class='text-center mt-2 text-gray-700'>{$fileData['name']}</p>";
     echo "</div>";
 }
 ?>
